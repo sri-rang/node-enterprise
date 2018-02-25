ec7110965d08299edca7e8ddcd25dddb977ce95c
Sri <srirangan@gmail.com>
2018-02-25 14:46:40 +0100

    config-management: rest api tests to verify nested hierarchies are created and persisted

--- config-management/tests/test_server.js
+++ config-management/tests/test_server.js
@@ -13,7 +13,7 @@ const failed = message => {
 
 const reset_timeout = () => {
     if (reset_timeout.timer) clearTimeout(reset_timeout.timer);
-    reset_timeout.timer = setTimeout(() => process.exit(0), 500);
+    reset_timeout.timer = setTimeout(() => process.exit(0), 5000);
 };
 
 const data_source_path = '/tmp/config_management_test_server.json';
@@ -58,7 +58,7 @@ instance.listen(3000, () => {
         .then(() => reset_timeout())
         .catch(() => failed('should update foo bar'));
 
-    // should read hello world and foo bar
+    // should verify state
     fetch('http://localhost:3000')
         .then(res => res.text())
         .then(res => body = res)
@@ -74,11 +74,27 @@ instance.listen(3000, () => {
         .then(() => reset_timeout())
         .catch(() => failed('should remove foo bar'));
 
-    // foo bar must be removed
+    // should verify state
     fetch('http://localhost:3000')
         .then(res => res.text())
         .then(res => body = res)
         .then(() => assert(body === '{"hello":"world","foo":null}'))
         .then(() => reset_timeout())
         .catch(() => failed('foo bar must be removed'));
+
+    // should add hello.world.foo bar
+    fetch('http://localhost:3000/she.sells.sea', { method: 'post', body: 'shells' })
+        .then(res => res.json())
+        .then(res => body = res)
+        .then(() => assert(body))
+        .then(() => reset_timeout())
+        .catch(() => failed('should add hello.world.foo bar'));
+
+    // should verify state
+    fetch('http://localhost:3000')
+        .then(res => res.text())
+        .then(res => body = res)
+        .then(() => assert(body === '{"hello":"world","foo":null,"she":{"sells":{"sea":"shells"}}}'))
+        .then(() => reset_timeout())
+        .catch(() => failed('should read hello world and foo bar'));
 });
